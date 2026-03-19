52db04436990d0f7161ef72282b3d9707e180329
Patrick Ronning <pcronning@pm.me>
2026-03-19 16:47:13 -0700

    feat: add Flutter mobile app (jobsync_mobile) - Tasks 1-7 complete

---
+++ jobsync_mobile/android/app/src/main/kotlin/com/ronningsystems/jobsync_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ronningsystems.jobsync_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ jobsync_mobile/test/widget_test.dart
@@ -0,0 +1,10 @@
+// Basic Flutter widget test placeholder
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('App builds successfully', (WidgetTester tester) async {
+    // Placeholder test - actual tests will be added with features
+    expect(true, isTrue);
+  });
+}
